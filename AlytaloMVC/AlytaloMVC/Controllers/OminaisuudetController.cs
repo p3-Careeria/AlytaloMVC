092a7e3678bb03306ec053a0146bb1704d8f657c
p3-Careeria <petri.laine@student.careeria.fi>
2019-12-18 19:00:41 +0200

    Saunan lämmitys toteutettu

--- AlytaloMVC/AlytaloMVC/Controllers/OminaisuudetController.cs
+++ AlytaloMVC/AlytaloMVC/Controllers/OminaisuudetController.cs
@@ -5,26 +5,29 @@ using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Windows.Threading;
 
 namespace AlytaloMVC.Controllers
 {
     public class OminaisuudetController : Controller
     {
-        private int i = 0;
+        private int talonLampo = 20;
+
+        // Tarvitsetko edes toista ajastinta? laitat alla olevaan metodikutsun joka 
+        // lisää arvoja tietyn ehdon verran ... 
+
+        // VITTTOOOO .. ei voi tallentaa uutta arvoa koska toi getOminaisuudet jää käyntiin 
+        // säätö uuteen ikkunaan, jolloin toi getOminaisuudet pysähtyy siksi aikaa?? 
         // GET: Ominaisuudet
         public ActionResult Index()
         {
-            this.i++; 
             return View();
-
         }
 
         [HttpPost]
         public ActionResult GetOminaisuudet()
         {
-            i++;
-            Debug.WriteLine("Kutsu toimii: "+i);
+            
             AlytaloEntities entities = new AlytaloEntities();
 
             var taulut = new OminaisuusViewModel
@@ -41,12 +44,12 @@ namespace AlytaloMVC.Controllers
                      "window.location.href ='/home/LuoOminaisuus' ;" +
                      "</script>");
             }
-            entities.Dispose(); 
+
+            entities.Dispose();
+            SaunaLampo();
             return PartialView("OminaisuudetPartial", taulut);
 
         }
-
-
         public ActionResult PoistaOminaisuus(int? id)
         {
             if (id != null)
@@ -64,6 +67,57 @@ namespace AlytaloMVC.Controllers
                             "window.location.href ='/Ominaisuudet' ;" +
                               "</script>");
         }
+        public ActionResult SetSauna(int? id)
+        {
+
+            AlytaloEntities entities = new AlytaloEntities();
+
+            Sauna sauna = entities.Sauna.Find(id);
+            if (sauna == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                Loki uusiKirjaus = new Loki();
+                uusiKirjaus.OminaisuusId = sauna.OminaisuusId.Value;
+                uusiKirjaus.Ajakohta = DateTime.Now; 
+
+                if (!sauna.Kaynnissa.Value)
+                {
+                    uusiKirjaus.Tapahtuma = "Sauna käynnistetty";                        
+                    sauna.Kaynnissa = true;
+
+                }
+                else if (sauna.Kaynnissa.Value)
+                {
+                    uusiKirjaus.Tapahtuma = "Sauna sammutettu";
+                    sauna.Kaynnissa = false;
+                }
+                entities.Loki.Add(uusiKirjaus);
+                entities.SaveChanges();
+            }
+            return View("Index");
+        }
+        private void SaunaLampo()
+        {
+            AlytaloEntities entities = new AlytaloEntities();
+            foreach (Sauna sauna in entities.Sauna)
+            {
+                if (sauna.Kaynnissa.Value && sauna.Lampo<100)
+                {
+                    int uusiLampo = sauna.Lampo.Value + 1;
+                    sauna.Lampo = uusiLampo;
+                } else if (!sauna.Kaynnissa.Value && sauna.Lampo>talonLampo) {
+                    int uusiLampo = sauna.Lampo.Value - 1;
+                    sauna.Lampo = uusiLampo;
+                }
+
+            }
+            entities.SaveChanges();
+            entities.Dispose(); 
+        }
+
 
     }
 
